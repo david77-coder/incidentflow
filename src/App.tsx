b6af666f2ebdb53f5ad1c33bc2fc328a4c2d0f5d
David <davidzgdcp@gmail.com>
2026-06-03 16:59:47 +0200

    Inicio proyecto IncidentFlow

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>🚀 IncidentFlow</h1>
+
+      <h2>Proyecto React + TypeScript</h2>
+
+      <p>
+        Aplicación de gestión de incidencias para portfolio profesional.
+      </p>
+    </div>
+  );
+}
+
+export default App;
